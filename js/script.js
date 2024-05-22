4503eec733214bc533f276589278a01ee208cf85
muhammadyofal <yofal.afz@gmail.com>
2024-05-22 12:38:27 +0700

    script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-function hasil() {
-    let alas= document.getElementById(alas).value;
-    let sisi1= document.getElementById(sisi1).value;
-    let sisi2= document.getElementById(sisi2).value;
+function hasilKeliling() {
+    let alas = document.getElementById("alas").value;
+    let sisi1 = document.getElementById("sisi1").value;
+    let sisi2 = document.getElementById("sisi2").value;
 
-    let keliling= perseint(alas) + perseint(sisi1) + perseint(sisi2)
-    document.getElementById(keliling).value=keliling;
-
-    let hasilkeliling= document.getElementById("hasilkeliling");
+    let keliling = alas + sisi1 + sisi2;
+    document.getElementById(
+        "hasilsegitigakeliling"
+    ).innerHTML = `keliling : <br> ${alas} + ${sisi1} + ${sisi2} = ${keliling}`;
 }
+
+function hasilLuas() {
+    let bawah = document.getElementById("bawah").value;
+    let tinggi = document.getElementById("tinggi").value;
+    let luas = 0.5 * bawah * tinggi;
+
+    document.getElementById(
+        "luasHasil"
+    ).innerHTML = `Luas: <br> ${0.5} * ${bawah} * ${tinggi} = ${luas}`;
+}
+
+document.querySelectorAll('a.scroll').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
